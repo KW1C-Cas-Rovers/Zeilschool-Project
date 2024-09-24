7e0c4402aa2255be951e97e627e789e4c4f2b99e
a-gfld <arjen.gfld+github@gmail.com>
2024-09-24 12:12:43 +0200

    + - basic calendar function + - Installed scss !!!NOT DONE!!!

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
 window.axios = axios;
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+
+import { Calendar } from "@fullcalendar/core";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import("@fullcalendar/daygrid/main.css");
+
+window.calendar = Calendar;
+window.dayGridPlugin = dayGridPlugin;
